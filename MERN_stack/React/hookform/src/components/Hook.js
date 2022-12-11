c7bf3b03739dc9e943f0ad82360189f97465a018
Ameer Baghdadi <ameer.r.baghdadi@gmail.com>
2022-12-11 13:26:35 +0200

    done

--- MERN_stack/React/hookform/src/components/Hook.js
+++ MERN_stack/React/hookform/src/components/Hook.js
@@ -21,25 +21,60 @@ const Hook = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [fNameError, setFNameError] = useState("");
+    const [lNameError, setLNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
     const firstName = (e) => {
         setFName(e.target.value);
+        if(e.target.value.length < 2 && e.target.value.length > 0) {
+            setFNameError("First Name must be 2 characters or longer!");
+        }
+        else {
+            setFNameError("");
+        }
     }
 
     const lastName = (e) => {
         setLName(e.target.value);
+        if(e.target.value.length < 2 && e.target.value.length > 0) {
+            setLNameError("Last Name must be 2 characters or longer!");
+        }
+        else {
+            setLNameError("");
+        }
     }
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
+        if(e.target.value.length < 5 && e.target.value.length > 0) {
+            setEmailError("Email must be at least 5 characters!");
+        }
+        else {
+            setEmailError("");
+        }
     }
 
     const handlePassword = (e) => {
         setPassword(e.target.value);
+        if(e.target.value.length < 8 && e.target.value.length > 0) {
+            setPasswordError("Passwords must be at least 8 characters!");
+        }
+        else {
+            setPasswordError("");
+        }
     }
 
     const handleConfirmPassword = (e) => {
         setConfirmPassword(e.target.value);
+        if(e.target.value.length < 8 && e.target.value.length > 0) {
+            setConfirmPasswordError("Passwords must be at least 8 characters!");
+        }
+        else {
+            setConfirmPasswordError("");
+        }
     }
 
   return (
@@ -47,18 +82,24 @@ const Hook = () => {
       <form style={formStyle}>
         <label>First Name:</label>
         <input type="text" onChange={firstName}/>
+        <p>{fNameError}</p>
 
         <label>Last Name:</label>
         <input type="text" onChange={lastName}/>
+        <p>{lNameError}</p>
         
         <label>Email:</label>
         <input type="email" onChange={handleEmail}/>
+        <p>{emailError}</p>
 
         <label>Password:</label>
         <input type="password" onChange={handlePassword}/>
+        <p>{passwordError}</p>
 
         <label>Confirm Password:</label>
         <input type="password" onChange={handleConfirmPassword}/>
+        <p>{confirmPasswordError}</p>
+
       </form>
         <h3>Your Form Data</h3>
         <p>First Name: {fName}</p>
