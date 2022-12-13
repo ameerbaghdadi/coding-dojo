c21e43bbdddb21b8ebac41947b114b7b99a7433c
Ameer Baghdadi <ameer.r.baghdadi@gmail.com>
2022-12-13 14:44:06 +0200

    Pokemon API Done

--- MERN_stack/React/pokemonapi/src/components/PokemonAPI.jsx
+++ MERN_stack/React/pokemonapi/src/components/PokemonAPI.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PokemonAPI = () => {
+    const [pokemon, setPokemon] = useState([]);
+    const [status, setStatus] = useState(false);
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=870&offset=20')
+        .then(response => response.json())
+        .then(response => setPokemon(response.results))
+    }, []);
+
+    const showPokemon = (e) => {
+       console.log(e);
+       setStatus(true);
+    }
+
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Hi</h1>
+      <button onClick={showPokemon}>Fetch Pokemon</button>
+      {status && pokemon.map((person, index)=>{
+                return (<div key={index}>{person.name}</div>)
+            })}
     </div>
   )
 }
